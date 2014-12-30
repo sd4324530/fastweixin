4a4c2c3785aeb23103bcc7cce017fdbf030a2062
PeiYu <125331682@qq.com>
2014-12-30 22:42:27 +0800

    调用API返回错误时，除了可以得到错误码，还可以得到官方给出的中文描述而不是冷冰冰的英文描述

--- src/main/java/com/github/sd4324530/fastweixin/api/response/BaseResponse.java
+++ src/main/java/com/github/sd4324530/fastweixin/api/response/BaseResponse.java
@@ -1,7 +1,9 @@
 package com.github.sd4324530.fastweixin.api.response;
 
 import com.github.sd4324530.fastweixin.api.entity.Model;
+import com.github.sd4324530.fastweixin.api.enums.ResultType;
 import com.github.sd4324530.fastweixin.util.JSONUtil;
+import com.github.sd4324530.fastweixin.util.StrUtil;
 
 /**
  * 微信API响应报文对象基类
@@ -22,7 +24,11 @@ public class BaseResponse implements Model {
     }
 
     public String getErrmsg() {
-        return errmsg;
+        //将接口返回的错误信息转换成中文，方便提示用户出错原因
+        if(StrUtil.isNotBlank(this.errcode)) {
+            return ResultType.get(this.errcode).getDescription();
+        }
+        return this.errmsg;
     }
 
     public void setErrmsg(String errmsg) {
