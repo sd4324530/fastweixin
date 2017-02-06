cddcb9075c7672c3ed3f42cc2096c209f05cc98a
wuwentao <wentao_wu@qq.com>
2017-02-06 16:20:24 +0800

    topcolor已经Deprecated了，在sendTemplateMsg的时候去掉requireNonNull 的检验

--- src/main/java/com/github/sd4324530/fastweixin/api/TemplateMsgAPI.java
+++ src/main/java/com/github/sd4324530/fastweixin/api/TemplateMsgAPI.java
@@ -71,7 +71,7 @@ public class TemplateMsgAPI extends BaseAPI {
         BeanUtil.requireNonNull(msg.getTouser(), "openid is null");
         BeanUtil.requireNonNull(msg.getTemplateId(), "template_id is null");
         BeanUtil.requireNonNull(msg.getData(), "data is null");
-        BeanUtil.requireNonNull(msg.getTopcolor(), "top color is null");
+//        BeanUtil.requireNonNull(msg.getTopcolor(), "top color is null");
 //        BeanUtil.requireNonNull(msg.getUrl(), "url is null");
         String url = BASE_API_URL + "cgi-bin/message/template/send?access_token=#";
         BaseResponse r = executePost(url, msg.toJsonString());
